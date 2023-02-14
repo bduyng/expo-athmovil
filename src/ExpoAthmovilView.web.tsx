7eefc0db54482f1cd86a556e568a81d8f624c8d8
bduyng <bduyng@gmail.com>
2023-02-14 16:06:59 +0700

    remove web support

--- src/ExpoAthmovilView.web.tsx
+++ src/ExpoAthmovilView.web.tsx
@@ -5,7 +5,6 @@ import { ExpoAthmovilViewProps } from './ExpoAthmovil.types';
 export default function ExpoAthmovilView(props: ExpoAthmovilViewProps) {
   return (
     <div>
-      <span>{props.name}</span>
     </div>
   );
 }
